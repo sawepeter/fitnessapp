251863674ecf0abadff4737d212d85ba14f66a9b
sawepeter <sawepeter6@gmail.com>
2023-04-24 22:02:51 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import { Box } from '@mui/material';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import ExerciseDetail from './pages/ExerciseDetail';
-
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+ 
 const App = () => {
     return ( 
-        <Box width="400px">
+        <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
             <Navbar />
 
             <Routes>
                 <Route path="/" element={ <Home />} />
                 <Route path="/exercise/:id" element={<ExerciseDetail />} />
             </Routes>
+
+            <Footer />
         </Box>
      );
 }
