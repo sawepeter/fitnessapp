28d66a59f4e2c31df7a36ae9632a9033d13cf3d4
sawepeter <sawepeter6@gmail.com>
2023-04-25 03:55:34 +0300

    HeroBanner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Box } from '@mui/material';
+
+
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <Exercises />
+      <SearchExercises />
+    </Box>
   )
 }
 
-export default Home
+export default Home;
